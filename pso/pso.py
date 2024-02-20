b729ad5f93faa7b6d91f2f2cf42f1e4e2d4aa86e
Russo <rafamega@live.com>
2024-02-20 17:46:30 -0300

    style: PEP 8 enforcing.

--- pso/pso.py
+++ pso/pso.py
@@ -1,163 +1,166 @@
-#------------------------------------------------------------------------------+
+# ------------------------------------------------------------------------------+
 #
 #	Parsa Dahesh (dinoco.parsa23@gmail.com or parsa.dahesh@studio.unibo.it)
 #	Racing Line Optimization with PSO
 #	MIT License, Copyright (c) 2021 Parsa Dahesh
 #
-#------------------------------------------------------------------------------+
+# ------------------------------------------------------------------------------+
 
 import random
 import time
 
+
 class Particle:
-	def __init__(self, n_dimensions, boundaries):
-		self.position = []
-		self.best_position = []
-		self.velocity = []
+    def __init__(self, n_dimensions, boundaries):
+        self.position = []
+        self.best_position = []
+        self.velocity = []
 
-		for i in range(n_dimensions):
-			self.position.append(random.uniform(0, boundaries[i]))
-			self.velocity.append(random.uniform(-boundaries[i], boundaries[i]))
+        for i in range(n_dimensions):
+            self.position.append(random.uniform(0, boundaries[i]))
+            self.velocity.append(random.uniform(-boundaries[i], boundaries[i]))
 
-		self.best_position = self.position
+        self.best_position = self.position
 
-	def update_position(self, newVal):
-		self.position = newVal
+    def update_position(self, newVal):
+        self.position = newVal
 
-	def update_best_position(self, newVal):
-		self.best_position = newVal
-	
-	def update_velocity(self, newVal):
-		self.velocity = newVal
+    def update_best_position(self, newVal):
+        self.best_position = newVal
+
+    def update_velocity(self, newVal):
+        self.velocity = newVal
+
 
 def optimize(cost_func, n_dimensions, boundaries, n_particles, n_iterations, w, cp, cg, verbose=False):
-	''' Particle Swarm Optimization
+    ''' Particle Swarm Optimization
 
-	This function will minimize the cost function
+    This function will minimize the cost function
 
-	Parameters
-	----------
-	cost_func : function
-		A function that will evaluate a given input, return a float value
-	n_dimension : int
-		Dimensionality of the problem
-	boundaries : list[float]
-		Problem's search space boundaries
-	n_particles : int
-		Number of particles
-	n_iteration : int
-		Number of iterations
-	w : float
-		Inertia parameter
-	cp : float
-		Constant parameter influencing the cognitive component (how much the current particle's best position will influnce its next iteration)
-	cg : float
-		Constant parameter influencing the social component (how much the global solution will influnce its next iteration of a particle)
-	verbose : bool
-		Flag to turn on output prints (default is False)
+    Parameters
+    ----------
+    cost_func : function
+        A function that will evaluate a given input, return a float value
+    n_dimension : int
+        Dimensionality of the problem
+    boundaries : list[float]
+        Problem's search space boundaries
+    n_particles : int
+        Number of particles
+    n_iteration : int
+        Number of iterations
+    w : float
+        Inertia parameter
+    cp : float
+        Constant parameter influencing the cognitive component (how much the current particle's best position will influnce its next iteration)
+    cg : float
+        Constant parameter influencing the social component (how much the global solution will influnce its next iteration of a particle)
+    verbose : bool
+        Flag to turn on output prints (default is False)
 
-	Returns
-	-------
-	global_solution :
-		Solution of the optimization
-	gs_eval :
-		Evaluation of global_solution with cost_func
-	gs_history :
-		List of the global solution at each iteration of the algorithm
-	gs_eval_history :
-		List of the global solution's evaluation at each iteration of the algorithm
-	'''
-	
-	particles = []
-	global_solution = []
-	gs_eval = []
-	gs_history = []
-	gs_eval_history = []
+    Returns
+    -------
+    global_solution :
+        Solution of the optimization
+    gs_eval :
+        Evaluation of global_solution with cost_func
+    gs_history :
+        List of the global solution at each iteration of the algorithm
+    gs_eval_history :
+        List of the global solution's evaluation at each iteration of the algorithm
+    '''
 
-	if verbose:
-		print()
-		print("------------------ PARAMETERS -----------------")
-		print("Number of dimensions:", n_dimensions)
-		print("Number of iterations:", n_iterations)
-		print("Number of particles:", n_particles)
-		print("w: {}\tcp: {}\tcg: {}".format(w,cp,cg))
-		print()
-		print("----------------- OPTIMIZATION ----------------")
-		print("Population initialization...")
-	
-	for i in range(n_particles):
-		particles.append(Particle(n_dimensions, boundaries))
+    particles = []
+    global_solution = []
+    gs_eval = []
+    gs_history = []
+    gs_eval_history = []
 
-	global_solution = particles[0].position
-	gs_eval = cost_func(global_solution)
-	for p in particles:		
-		p_eval = cost_func(p.best_position)
-		if p_eval < gs_eval:
-			global_solution = p.best_position
-			gs_eval = cost_func(global_solution)
+    if verbose:
+        print()
+        print("------------------ PARAMETERS -----------------")
+        print("Number of dimensions:", n_dimensions)
+        print("Number of iterations:", n_iterations)
+        print("Number of particles:", n_particles)
+        print("w: {}\tcp: {}\tcg: {}".format(w, cp, cg))
+        print()
+        print("----------------- OPTIMIZATION ----------------")
+        print("Population initialization...")
 
-	gs_history.append(global_solution)
-	gs_eval_history.append(gs_eval)
-	
-	if verbose:
-		print("Start of optimization...")
-		printProgressBar(0, n_iterations, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    for i in range(n_particles):
+        particles.append(Particle(n_dimensions, boundaries))
 
-	start_time = time.time_ns()
+    global_solution = particles[0].position
+    gs_eval = cost_func(global_solution)
+    for p in particles:
+        p_eval = cost_func(p.best_position)
+        if p_eval < gs_eval:
+            global_solution = p.best_position
+            gs_eval = cost_func(global_solution)
 
-	for k in range(n_iterations):
-		for p in particles:
-			rp = random.uniform(0,1)
-			rg = random.uniform(0,1)
+    gs_history.append(global_solution)
+    gs_eval_history.append(gs_eval)
 
-			velocity = []
-			new_position =[]
-			for i in range(n_dimensions):
-				velocity.append(w * p.velocity[i] + \
-        			cp * rp * ( p.best_position[i] - p.position[i] ) + \
-					cg * rg * ( global_solution[i] - p.position[i] ))
+    if verbose:
+        print("Start of optimization...")
+        printProgressBar(0, n_iterations, prefix='Progress:', suffix='Complete', length=50)
 
-				if velocity[i] < -boundaries[i]:
-					velocity[i] = -boundaries[i]
-				elif velocity[i] > boundaries[i]:
-					velocity[i] = boundaries[i]
+    start_time = time.time_ns()
 
-				new_position.append(p.position[i] + velocity[i])
-				if new_position[i] < 0.0:
-					new_position[i] = 0.0
-				elif new_position[i] > boundaries[i]:
-					new_position[i] = boundaries[i]
+    for k in range(n_iterations):
+        for p in particles:
+            rp = random.uniform(0, 1)
+            rg = random.uniform(0, 1)
 
-			p.update_velocity(velocity)
-			p.update_position(new_position)
+            velocity = []
+            new_position = []
+            for i in range(n_dimensions):
+                velocity.append(w * p.velocity[i] + \
+                                cp * rp * (p.best_position[i] - p.position[i]) + \
+                                cg * rg * (global_solution[i] - p.position[i]))
 
-			p_eval = cost_func(p.position)
-			if p_eval < cost_func(p.best_position):
-				p.update_best_position(p.position)
-				if p_eval < gs_eval:
-					global_solution = p.position
-					gs_eval = p_eval
-					
-		gs_eval_history.append(gs_eval)
-		gs_history.append(global_solution)
+                if velocity[i] < -boundaries[i]:
+                    velocity[i] = -boundaries[i]
+                elif velocity[i] > boundaries[i]:
+                    velocity[i] = boundaries[i]
 
-		if verbose:
-			printProgressBar(k+1, n_iterations, prefix = 'Progress:', suffix = 'Complete', length = 50)
-	
-	finish_time = time.time_ns()
-	elapsed_time = (finish_time-start_time)/10e8
-	
-	if verbose:
-		time.sleep(0.2)
-		print("End of optimization...")
-		print()
-		print("------------------- RESULTS -------------------")
-		print("Optimization elapsed time: {:.2f} s".format(elapsed_time))
-		print("Solution evaluation: {:.5f}".format(gs_eval))
+                new_position.append(p.position[i] + velocity[i])
+                if new_position[i] < 0.0:
+                    new_position[i] = 0.0
+                elif new_position[i] > boundaries[i]:
+                    new_position[i] = boundaries[i]
 
-	return global_solution, gs_eval, gs_history, gs_eval_history
+            p.update_velocity(velocity)
+            p.update_position(new_position)
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+            p_eval = cost_func(p.position)
+            if p_eval < cost_func(p.best_position):
+                p.update_best_position(p.position)
+                if p_eval < gs_eval:
+                    global_solution = p.position
+                    gs_eval = p_eval
+
+        gs_eval_history.append(gs_eval)
+        gs_history.append(global_solution)
+
+        if verbose:
+            printProgressBar(k + 1, n_iterations, prefix='Progress:', suffix='Complete', length=50)
+
+    finish_time = time.time_ns()
+    elapsed_time = (finish_time - start_time) / 10e8
+
+    if verbose:
+        time.sleep(0.2)
+        print("End of optimization...")
+        print()
+        print("------------------- RESULTS -------------------")
+        print("Optimization elapsed time: {:.2f} s".format(elapsed_time))
+        print("Solution evaluation: {:.5f}".format(gs_eval))
+
+    return global_solution, gs_eval, gs_history, gs_eval_history
+
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -173,7 +176,7 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
     # Print New Line on Complete
-    if iteration == total: 
+    if iteration == total:
         print()
